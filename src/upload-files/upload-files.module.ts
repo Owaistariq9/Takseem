09ad2da083108d47863ed2a090f97492ad39b464
Owaistariq9 <72124368+Owaistariq9@users.noreply.github.com>
2022-12-12 18:36:29 +0500

    Delete src directory

--- src/upload-files/upload-files.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UploadFilesService } from './upload-files.service';
-import { UploadFilesController } from './upload-files.controller';
-
-@Module({
-  providers: [UploadFilesService],
-  controllers: [UploadFilesController]
-})
-export class UploadFilesModule {}
--- src/vehicles/vehicles.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { VehicleService } from './vehicles.service';
-import { VehiclesController } from './vehicles.controller';
-
-@Module({
-  providers: [VehicleService],
-  controllers: [VehiclesController]
-})
-export class VehicleModule {}
